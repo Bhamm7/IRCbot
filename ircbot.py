c6b565ee8789d4205a6eec9b22bfef049ecb688f
Brett Hamm <brett.hamm@gmail.com>
2018-04-10 18:03:09 -0600

    Add files via upload

---
+++ ircbot.py
@@ -0,0 +1,70 @@
+import socket, string, re, time
+
+################## CONFIGURATION DATA ###################
+#########################################################
+
+# Don't touch these two
+SERVER = 'irc.twitch.tv'
+PORT = 6667
+# These two are your twitch channel name! (Defaults to lowercase)
+NICKNAME = 'Trancefury'
+USERNAME = 'Trancefury'
+# oAuth token generated from connecting your twitch at https://twitchapps.com/tmi/
+PASSWORD = 'oauth:5nnti14gcr18214zsq23wt7728e5h7'
+# Channel name.
+CHANNEL = 'electricalskateboard'
+# How many seconds between your spams
+INTERVAL = 5
+# Total seconds to spam for
+HOST_DURATION = 60
+# Spam command
+COMMAND = '!hostme'
+
+#########################################################
+#########################################################
+
+
+
+IRC = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+def Connect():
+    print("Connecting to server " + SERVER + ", as user " + USERNAME + ", with password " + PASSWORD + "to channel " + CHANNEL + "\n")
+    IRC.connect((SERVER, PORT))
+    SendData("PASS " + PASSWORD)
+    SendData("USER " + USERNAME)
+    SendData("NICK " + NICKNAME)
+    channel = '#' + CHANNEL
+    SendData("JOIN %s" % channel)
+
+def SendData(command):
+    IRC.send(bytes(command + '\n', 'utf-8'))
+    # IRC.send("PRVMSG #" + CHANNEL + ":" + command + "\n")
+
+def SpamHost():
+    print("Hosting lottery active, spamming command: " + COMMAND + "every " + str(INTERVAL) + "second(s) for total duration of " + str(HOST_DURATION))
+    startTime = time.time()
+    currentTime = startTime
+    while ((currentTime - startTime) <= HOST_DURATION):
+        SendData("PRIVMSG #" + CHANNEL + " :" + COMMAND)
+        print('Sent command: ' + COMMAND)
+        time.sleep(INTERVAL)
+        currentTime = time.time()
+    print("Hosting period ended.\n")
+
+
+### Main ###
+Connect()
+
+while (1):
+    buffer = IRC.recv(2048)
+    text = buffer.decode('utf8', "ignore")
+    # if re.search("PING", text): #check if server have sent ping command
+    # if text[0] == "PING":
+    #     send_data("PONG %s" % text[1]) #answer with pong as per RFC 1459
+    # if re.search("^:(trancefury|electricalskateboard)", text):
+    #     print (text)
+
+    pattern = r':' + re.escape(CHANNEL) + r".+(lottery started|lottery in progress)"
+    if re.search(pattern, text):
+        SpamHost()
+    
